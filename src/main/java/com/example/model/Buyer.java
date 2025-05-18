fc9937b9c55856cf19d3dec011f58acdab7da20c
Ioana Enescu <ioana.enescu@student.upt.ro>
2025-05-18 23:36:59 +0300

    adaugare clasa Buyer

---
+++ src/main/java/com/example/model/Buyer.java
@@ -0,0 +1,21 @@
+package com.example.model;
+
+public class Buyer {
+    private String username;
+    private String email;
+    private String password;
+
+    public Buyer(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
