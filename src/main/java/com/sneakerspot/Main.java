c2bb1cae58afc847516f28e53f4e57fbbd8e6e71
Ioana Enescu <ioana.enescu@student.upt.ro>
2025-06-01 22:03:45 +0300

    Adaugare clase Login si Registration pentru ui

--- src/main/java/com/sneakerspot/Main.java
+++ src/main/java/com/sneakerspot/Main.java
@@ -1,9 +1,16 @@
 package com.sneakerspot;
 
-import com.sneakerspot.model.User;
+import com.sneakerspot.ui.auth.LoginScreen;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        ;
+        // Look & feel opțional
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {}
+
+        SwingUtilities.invokeLater(() -> new LoginScreen().setVisible(true));
     }
 }
